d20eb1bcfd0ab13b7e86fb18dcd9a492d6dc2240
maximpolovinkin <polloVinkin@yandex.ru>
2022-07-30 21:41:06 +0300

    beta version of functionality

--- ToDo List/ViewController.swift
+++ ToDo List/ViewController.swift
@@ -7,12 +7,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource {
+   
+   
+    let a = FileCache(a: 1)
+    
+
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Мои дела"
+        view.addSubview(table)
+        table.dataSource = self
+       // table.backgroundColor = UIColor.lightGray
+       
+        
+         
     }
+    
+    @objc func addTapped(sender: UIButton!){
+        let alert = UIAlertController(title: "Новое дело", message: "Добавить новую задачу", preferredStyle: .alert)
+        alert.addTextField{ field in
+            field.placeholder = "Что надо сделать?"
+        }
+        alert.addAction(UIAlertAction(title: "Готово", style: .default, handler:{ (_) in
+            if let field = alert.textFields?.first{
+                if let text = field.text, !text.isEmpty{
+                    
+                    self.a.addTask(id: "1", task: text, deadLine: nil, isCopmplete: false, createDate: Date.now)
+                    self.table.reloadData()
+                    
+                }
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Отменить", style: .cancel, handler: nil ))
+        present(alert, animated: true)
+    }
+    
+    @objc func addTask(){
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+        addButton()
+        
+        
+    }
+    
+    func addButton() {
+        let addButton = UIButton(frame: CGRect(x: (view.bounds.width / 2) - 24, y: view.bounds.height - 96 , width: 48, height: 48))
+        addButton.addTarget(self, action: #selector(addTapped), for: UIControl.Event.touchUpInside)
+        
+        addButton.setTitle("+", for: .normal)
+        addButton.backgroundColor = UIColor.systemBlue
+        addButton.titleLabel?.textColor = UIColor.white
+        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 38)
+        addButton.contentVerticalAlignment = .top
+        addButton.contentHorizontalAlignment = .center
+        addButton.layer.cornerRadius = addButton.layer.bounds.height / 2
+        view.addSubview(addButton)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return a.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.lightGray
+        cell.textLabel?.text = a.tasks[indexPath.row].task
+        return cell
+    }
+    
 
 
 }
